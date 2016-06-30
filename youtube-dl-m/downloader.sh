4f203579d128e164564bbf5fc8bd2ad998dcd3a1
Muqsit Nawaz <muqsitnawaz@gmail.com>
2016-06-30 11:08:03 -0700

    Graphs generated on direct connection

--- youtube-dl-m/downloader.sh
+++ youtube-dl-m/downloader.sh
@@ -1,9 +1,14 @@
 
 # 24 secs, 50 secs, 5 mins 16 secs
-Array=(
+urls=(
   'https://www.youtube.com/watch?v=J4qqnWscqeQ' 
   'https://www.youtube.com/watch?v=RwN7oeqN7XY', 
   'https://www.youtube.com/watch?v=Cxy88GeEAxg'
   )
 
 
+for i in "${urls[@]}"
+do
+  echo $i
+  
+done
