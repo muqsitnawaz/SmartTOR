0cee1497b334556e9eab9ff350410b15b9ff99b1
Muqsit Nawaz <muqsitnawaz@gmail.com>
2016-06-30 09:36:43 -0700

    File generator added. Pytomo logs generated

---
+++ node-server-m/generator.sh
@@ -0,0 +1,10 @@
+
+
+# 10000000 100000000 1000000000
+fileSizes=(10000 100000 1000000)
+
+for i in "${fileSizes[@]}"
+do
+	echo $i
+   	dd if=/dev/zero of=$i bs=$i count=1
+done
---
+++ psnr-exps-m/Image_Error_Measurements/calculator.m
@@ -0,0 +1,2 @@
+
+disp(PSNR(imread('talk_normal_snap.png', imread('talk_tor_snap.png'))))
